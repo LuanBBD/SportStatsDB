44d06c3755d22ee9156197d887633ed37ddd13b1
Lusanda Mthiba <61183124+luslindo@users.noreply.github.com>
2022-02-18 10:43:09 +0200

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -7,3 +7,15 @@ GO
 
 USE SportStatsDB;
 GO
+
+CREATE TABLE [dbo].[Manager](
+    [ManagerID] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName][varchar](120) NOT NULL,
+	[LastName] [varchar](120) NOT NULL,
+    [DateOfBirth] [date] NOT NULL,
+    [CountryOfBirth] [varchar](120) NOT NULL,
+);
+GO
+ALTER TABLE dbo.Manager
+ADD CONSTRAINT [PK_Manager] PRIMARY KEY CLUSTERED ([ManagerID] ASC);
+GO
