0d00a275644489a06ac345782bda088646d10ebb
Lusanda Mthiba <61183124+luslindo@users.noreply.github.com>
2022-02-23 12:05:11 +0200

    Update create_udf.sql

--- UDF/create_udf.sql
+++ UDF/create_udf.sql
@@ -1,1 +1,14 @@
+CREATE FUNCTION getPoints
+(
+@team_id int
+)
+RETURNS int
+AS
+BEGIN
+  DECLARE @points int
 
+  SELECT @points = (wins *3 + draws) FROM Team_Stats
+  WHERE team_id = @team_id
+
+  RETURN @points
+END
