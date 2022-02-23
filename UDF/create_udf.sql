efa0e11d36354d007a17a9ae462272805bc1778b
Lusanda Mthiba <61183124+luslindo@users.noreply.github.com>
2022-02-23 22:12:38 +0200

    Update create_udf.sql

--- UDF/create_udf.sql
+++ UDF/create_udf.sql
@@ -12,3 +12,18 @@ BEGIN
 
   RETURN @points
 END
+
+CREATE FUNCTION goalDifference
+(
+@team_id int
+)
+RETURNS int
+AS
+BEGIN
+  DECLARE @goal_difference int
+
+  SELECT @goal_difference = (goals_for - goals_against) FROM Team_Stats
+  WHERE team_id = @team_id
+
+  RETURN @goal_difference
+END
