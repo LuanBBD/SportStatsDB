119a0f3823ad5f8cf1178afaf7901f0366bc5a43
sphelelo <99885380+sphelelo@users.noreply.github.com>
2022-02-24 10:49:14 +0200

    Update player and player_league_stats tables

--- Tables/create_tables.sql
+++ Tables/create_tables.sql
@@ -67,13 +67,27 @@ CREATE TABLE Position(
 );
 GO
 
-CREATE TABLE Player(player_id INT PRIMARY KEY,team_id INT NOT NULL,player_name VARCHAR(128) NOT NULL,height INT NOT NULL,photo VARCHAR(250) NOT NULL,injury_status INT NOT NULL,position_id INT NOT NULL,player_league_stats_id INT)
+CREATE TABLE Player_League_Stats (
+	player_league_stats_id INT PRIMARY KEY NOT NULL,
+	league_id INT FOREIGN KEY REFERENCES League(league_id) NOT NULL,
+	goals INT NOT NULL,
+	yellow_cards TINYINT NOT NULL,
+	red_cards TINYINT NOT NULL,
+	appearances TINYINT NOT NULL
+);
 GO
-CREATE TABLE Player_league_stats(player_league_stats_id INT PRIMARY KEY,league_id INT,goals INT NOT NULL,yellow_cards INT NOT NULL,red_cards INT NOT NULL,appearances INT NOT NULL)
-GO
-ALTER TABLE Player
-    ADD CONSTRAINT fk_player_stats FOREIGN KEY(player_league_stats_id)
-    REFERENCES Player_league_stats(player_league_stats_id)
+
+CREATE TABLE Player (
+	player_id INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+	team_id INT FOREIGN KEY REFERENCES Team(team_id) NOT NULL,
+	player_league_stats_id INT FOREIGN KEY REFERENCES Player_League_Stats(player_league_stats_id) NOT NULL,
+	position_id INT FOREIGN KEY REFERENCES Position(position_id) NOT NULL,
+	first_name VARCHAR(128) NOT NULL,
+	last_name VARCHAR(128) NOT NULL,
+	height INT NOT NULL,
+	photo VARCHAR(250) NOT NULL,
+	health_status BIT DEFAULT 0, -- 0: Fit, 1: Injured	
+);
 GO
 
 CREATE TABLE Matches (
