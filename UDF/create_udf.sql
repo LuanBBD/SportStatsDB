460640b66f4b119ebd27a634bac877c9e22e9531
LuanBBD <99713447+LuanBBD@users.noreply.github.com>
2022-02-24 14:38:40 +0200

    Update getLeagueTable to use UDF's

--- UDF/create_udf.sql
+++ UDF/create_udf.sql
@@ -1,21 +1,18 @@
-
 CREATE FUNCTION getLeagueTable (@league_id INT)
 RETURNS TABLE
 AS
 RETURN
 SELECT
 t.team_name as Club,
-no_games_played as MP,
+dbo.getNumberOfGames(ts.team_id) as MP,
 wins as W,
 draws as D,
 losses as L,
 goals_for as GF,
 goals_against as GA,
-goals_for - goals_against as GD,
-(wins * 3 + draws) as Pts
+dbo.goalDifference(ts.team_id) as GD,
+dbo.getPoints(ts.team_id) as Pts
 FROM Team_Stats ts
 	INNER JOIN Team t on t.team_id = ts.team_id
 WHERE ts.league_id=@league_id;
 GO
-
-select * from getLeagueTable(2) order by Pts desc
